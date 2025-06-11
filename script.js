57103bdf3ed7d5ddd35911e0104d8a599b03ce5b
carfreaker1 <amanrosevalley@gmail.com>
2025-06-11 23:52:20 +0530

    add_footer

--- script.js
+++ script.js
@@ -98,3 +98,10 @@ const products = [
   
   // Initial render
   renderProducts();
+
+  const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('open');
+});
